dfd0950f3366716695077ffd3caacaded33bc483
bryan4027 <b.acosta@utexas.edu>
2022-05-11 16:25:56 -0500

    updated files

--- src/2draft.py
+++ src/2draft.py
@@ -27,14 +27,50 @@ def load_data():
         data = {}
         data['Candidates'] = []
         for row in csvReader:
-            if len(data['Candidates']) == 1:
+            if len(data['Candidates']) == 10:
                 break
             data['Candidates'].append(dict(row))
+        print('\ndata has been successfully loaded. \n')
+        return '\ndata has been successfully loaded. \n'
 
-        print(data)
+#@app.route('/help', methods=['GET'])
+def return_instructions():
+    '''
+    This Route returns all of the available commands and instructions on
+    how to use them.
+    '''
+    logging.info("Instructions on requesting data printed below.")
+    output = "/help - (GET) - output these instructions"
+    output = output + "\n/load_data - (POST) -  "
+    output = output + "\n/epoch - (GET) - Returns all EPOCHs. "
+    output = output + "\n/epoch/<epoch> - (GET) - R" 
 
 
+    return output
+
+#@app.route('/confirmed_planets', methods=['GET'])
+def return_confirmed_planets():
+    """
+    This route grabs all of the epochs and makes it a list.
+    Return: it returns the list of epochs
+    """
+    output = "\n"
+    logging.info("Looking for all of the Epoch Positions\n")
+    global epoch_length
+    global epoch_list #also output
+    global epoch_data
+    planet_list = "\n"
+    datasave = data['Candidates']
+    data_length = len(datasave)
+    num = 0
+    for i in range(data_length):        
+        current_planet = datasave[i]['kepler_name']
+        if current_planet is not "":
+            planet_list = planet_list + current_planet + '\n'
+    print('\nList of Confirmed Planets:\n',planet_list)
+    return planet_list
 
 
 if __name__ == '__main__':
     load_data()
+    return_confirmed_planets()
