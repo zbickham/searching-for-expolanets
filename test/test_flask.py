4350b76668ca1fa51fd7c91920779c899f1e6ea7
Zeke Bickham <98800270+zbickham@users.noreply.github.com>
2022-05-11 15:58:54 -0500

    Update test_flask.py

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,1 +1,18 @@
+import pytest
+import requests
 
+api_host = 'localhost'
+api_port = '5003'
+api_prefix = f'http://{api_host}:{api_port}'
+
+def test_data_laod():
+    route = f'{api_prefix}/download_data'
+    response = requests.post(route)
+    assert response.ok == True
+    assert response.content == b'Data has been loaded.\n'
+
+def test_data_get():
+    route = f'{api_prefix}/feature/mag'
+    response = requests.get(route)
+    
+    assert response.ok == True
