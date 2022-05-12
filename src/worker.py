90487655fa07222cc882882553c940ff29c9d955
Bryan Acosta <70548114+bryan4027@users.noreply.github.com>
2022-05-12 08:13:04 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,1 +1,25 @@
+import json
+import redis
+import matplotlib.pyplot as plt
 
+def makeplot(data):
+    
+
+    x_vals = []
+    y_vals = []
+    total = 0
+    for i in range(len(data)):
+        if data[i]['koi_disposition'] == 'CONFIRMED':
+            total +=1
+        year = data[i]['koi_time0bk']
+        y_vals.append(total)
+        x_vals.append(year)
+
+    plt.xlabel("Years")
+    plt.title("Visualization of confirmed Exoplanets over the years")
+    plt.ylabel("How Many Confirmed Exoplanets")
+    plt.plot(x_vals, y_vals, 'b--')
+    plt.savefig('/plot.png')
+    
+
+makeplot(data)
